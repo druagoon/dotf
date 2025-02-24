15c3d9ee29093208682976237e61ec6d67f258c1
lazyboy <lazyboy.fan@gmail.com>
2025-02-24 14:03:14 +0800

    feat(build): enhance Argcfile.sh with improved build and distribution processes

--- Argcfile.sh
+++ Argcfile.sh
@@ -19,8 +19,9 @@ SRC_DIR="${BASE_DIR}/src"
 SRC_MAIN="${SRC_DIR}/main.sh"
 BUILD_DIR="${BASE_DIR}/build"
 BUILD_TARGET="${BUILD_DIR}/${NAME}.sh"
+BIN_DIR="${BASE_DIR}/bin"
+BIN_TARGET="${BIN_DIR}/${NAME}"
 DIST_DIR="${BASE_DIR}/dist"
-DIST_TARGET="${DIST_DIR}/${NAME}"
 
 # Define the path to the AWK script used for shell inclusion.
 # The script is located in the base directory and is named 'shinc.awk'.
@@ -38,6 +39,14 @@ else
     echo "Unsupported OS: ${OS}" >&2
     exit 1
 fi
+
+debug() {
+    printf "\033[35m==>\033[0m \033[1m%s\033[0m\n" "$*"
+}
+
+hai() {
+    printf "  \033[34m==>\033[0m \033[1m%s\033[0m\n" "$*"
+}
 
 ensure_dir() {
     for dir in "$@"; do
@@ -95,47 +104,59 @@ toml::check() {
 
 # @cmd Compile and build binaries
 build() {
-    # Format the shell scripts in the source directory before building
+    debug "Building ${NAME}"
+
+    hai "Format sources: ${SRC_DIR}"
     fmt::src
 
-    # Compile source
     cd "${SRC_DIR}" || exit
-    ensure_dir "${BUILD_DIR}" "${DIST_DIR}"
+    ensure_dir "${BUILD_DIR}" "${BIN_DIR}"
+    hai "Compile source: ${SRC_MAIN} -> ${BUILD_TARGET}"
     awk -v version="${VERSION}" -f "${SHINC_AWK}" "${SRC_MAIN}" >"${BUILD_TARGET}"
     chmod +x "${BUILD_TARGET}"
     fmt_shell "${BUILD_TARGET}"
 
-    # Build without argc dependency
-    argc --argc-build "${BUILD_TARGET}" "${DIST_TARGET}"
-    fmt_shell "${DIST_TARGET}"
-    chmod +x "${DIST_TARGET}"
+    hai "Argc build: ${BUILD_TARGET} -> ${BIN_TARGET}"
+    argc --argc-build "${BUILD_TARGET}" "${BIN_TARGET}"
+    fmt_shell "${BIN_TARGET}"
+    chmod +x "${BIN_TARGET}"
 
-    # Generate shell completions
+    hai "Generate shell completions"
     generate_completions
 }
 
 # @cmd Test binaries
 test() {
-    ${DIST_TARGET} --version
+    debug "Testing ${NAME}"
+
+    ${BIN_TARGET} --version
 }
 
 # @cmd Pre-release (bump version, update CHANGELOG, and create a tag)
 # @meta require-tools git
 # @arg version!                 Version number
 prerelease() {
+    debug "Pre-release ${NAME}"
+
     local version="${argc_version}"
     local tag="v${version}"
     if git rev-parse "${tag}" >/dev/null 2>&1; then
         echo "tag already exists: ${tag}" >&2
         exit 1
     fi
-    echo "version: ${version}"
-    echo "tag: ${tag}"
+
+    hai "Version: ${version}"
+    hai "Tag: ${tag}"
+    hai "Update version"
     local argc_file="Argcfile.sh"
-    local changelog="CHANGELOG.md"
     ${SED} -i 's/^VERSION="[0-9]\+\.[0-9]\+\.[0-9]\+"$/VERSION="'"${version}"'"/' "${argc_file}"
     git add "${argc_file}"
+
+    hai "Update CHANGELOG"
+    local changelog="CHANGELOG.md"
     git cliff -o "${changelog}" -t "${tag}"
+
+    hai "Commit and tag"
     git add "${changelog}"
     git commit -m "chore: Release ${tag}"
     git tag -a -m "chore: Release ${tag}" "${tag}"
@@ -143,23 +164,34 @@ prerelease() {
 
 # @cmd Distribute binaries
 dist() {
-    if [[ ! -f ${DIST_TARGET} ]]; then
-        echo "Binary not found: ${DIST_TARGET}" >&2
+    debug "Distribute ${NAME}"
+
+    if [[ ! -f ${BIN_TARGET} ]]; then
+        echo "Binary not found: ${BIN_TARGET}" >&2
         exit 1
     fi
-    ensure_dir "${DIST_DIR}" bin
-    cp -v "${DIST_TARGET}" bin/
+
+    ensure_dir "${DIST_DIR}"
     local name="${NAME}-${TAG}.tar.gz"
     local fullname="${DIST_DIR}/${name}"
-    echo "Archive to: ${fullname}"
+    hai "Archive to: ${fullname}"
     tar -cvzf "${fullname}" README.md LICENSE bin/ contrib/
-    rm -rf bin/
-    cd "${DIST_DIR}" && sha256sum "${name}" >"${name}.sha256"
+
+    local checksum="${DIST_DIR}/${name}.sha256"
+    hai "Generate sha256sum: ${checksum}"
+    cd "${DIST_DIR}" && sha256sum "${name}" >"${checksum}"
 }
 
 # @cmd Clean up
 clean() {
-    rm -rf "${BUILD_DIR}" "${DIST_DIR}"
+    debug "Clean up"
+
+    local -a dirs=(
+        "${BIN_DIR}"
+        "${BUILD_DIR}"
+        "${DIST_DIR}"
+    )
+    rm -rf "${dirs[@]}"
 }
 
 # See more details at https://github.com/sigoden/argc
