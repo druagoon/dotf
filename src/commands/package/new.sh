c4fc41d2be71bd6c9ebe60211f062dc1ae858b2a
lazyboy <lazyboy.fan@gmail.com>
2025-03-04 00:00:32 +0800

    feat(package): the `package new` command supports multiple plugin names

--- src/commands/package/new.sh
+++ src/commands/package/new.sh
@@ -3,34 +3,37 @@
 # @flag      --no-omz-plugins                   Do not create oh-my-zsh plugins directory
 # @flag      --no-omz-functions                 Do not create oh-my-zsh functions directory
 # @flag      --no-omz-completions               Do not create oh-my-zsh completions directory
-# @option    --omz-plugin-name <NAME>           Name of the oh-my-zsh plugin
 # @arg name!                                    Name of the package
+# @arg plugin_names* <PLUGIN_NAME>              Name of the plugins
 package::new() {
     local name="${argc_name}"
     local pkg_dir="$(dotf::pkg::dir::get "${name}")"
     local pkg_omz_custom_dir="${pkg_dir}/.oh-my-zsh/custom"
     std::path::dir::ensure "${pkg_omz_custom_dir}"
 
-    if std::bool::is_false "${argc_no_omz_completions}"; then
+    if std::bool::is_false "${argc_no_omz_completions:-}"; then
         std::path::dir::ensure "${pkg_omz_custom_dir}/completions"
     fi
-    if std::bool::is_false "${argc_no_omz_functions}"; then
+    if std::bool::is_false "${argc_no_omz_functions:-}"; then
         std::path::dir::ensure "${pkg_omz_custom_dir}/functions"
     fi
-    if std::bool::is_false "${argc_no_omz_plugins}"; then
-        local pkg_omz_plugin_name="${argc_omz_plugin_name:-"${name}"}"
-        if std::bool::is_false "${argc_no_omz_plugin_prefix}"; then
-            pkg_omz_plugin_name="${DF_OMZ_PLUGIN_PREFIX}${pkg_omz_plugin_name}"
+    if std::bool::is_false "${argc_no_omz_plugins:-}"; then
+        local -a pkg_omz_plugin_names=("${argc_plugin_names[@]:-"${name}"}")
+        if std::bool::is_false "${argc_no_omz_plugin_prefix:-}"; then
+            pkg_omz_plugin_names=("${pkg_omz_plugin_names[@]/#/${DF_OMZ_PLUGIN_PREFIX}}")
         fi
-        local pkg_omz_plugin_dir="${pkg_omz_custom_dir}/plugins/${pkg_omz_plugin_name}"
-        std::path::dir::ensure "${pkg_omz_plugin_dir}/bin"
+        local pkg_omz_plugin_dir pkg_omz_plugin_file pkg_omz_plugin_readme
+        for plg_name in "${pkg_omz_plugin_names[@]}"; do
+            pkg_omz_plugin_dir="${pkg_omz_custom_dir}/plugins/${plg_name}"
+            std::path::dir::ensure "${pkg_omz_plugin_dir}/bin"
 
-        local pkg_omz_plugin_file="${pkg_omz_plugin_dir}/${pkg_omz_plugin_name}.plugin.zsh"
-        std::path::file::ensure "${pkg_omz_plugin_file}"
+            pkg_omz_plugin_file="${pkg_omz_plugin_dir}/${plg_name}.plugin.zsh"
+            std::path::file::ensure "${pkg_omz_plugin_file}"
 
-        local pkg_omz_plugin_readme="${pkg_omz_plugin_dir}/README.md"
-        if [[ ! -f "${pkg_omz_plugin_readme}" ]]; then
-            printf "# ${pkg_omz_plugin_name} plugin\n" >"${pkg_omz_plugin_readme}"
-        fi
+            pkg_omz_plugin_readme="${pkg_omz_plugin_dir}/README.md"
+            if [[ ! -f "${pkg_omz_plugin_readme}" ]]; then
+                printf "# ${plg_name} plugin\n" >"${pkg_omz_plugin_readme}"
+            fi
+        done
     fi
 }
